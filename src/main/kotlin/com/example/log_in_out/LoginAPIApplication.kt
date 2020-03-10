417b6581563c52f37c98e7b7e064e971aca85222
a783953 <tanws-wa16@student.tarc.edu.my>
2020-03-10 14:36:22 +0800

    google cloud

--- src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
+++ src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
@@ -2,6 +2,7 @@ package com.example.log_in_out
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
