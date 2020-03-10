7d85e752fe3626cef7e8c8f0615392af7adbef93
a783953 <tanws-wa16@student.tarc.edu.my>
2020-03-10 14:48:18 +0800

    google cloud

--- src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
+++ src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
@@ -3,14 +3,12 @@ package com.example.log_in_out
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-open class LoginAPIApplication {
-	companion object {
-		@JvmStatic
-		fun main(args: Array<String>) {
-			SpringApplication.run(LoginAPIApplication::class.java, *args)
-		}
-	}
+open class LoginAPIApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoginAPIApplication>(*args)
 }
