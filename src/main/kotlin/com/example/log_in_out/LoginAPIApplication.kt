dccfcad91b76ee91deec4af7eb6c5deecd07088a
a783953 <tanws-wa16@student.tarc.edu.my>
2020-03-11 11:51:29 +0800

    google cloud database connection

--- src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
+++ src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
@@ -1,7 +1,6 @@
 package com.example.log_in_out
 
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.storage.StorageOptions
 import com.google.common.collect.Lists
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
