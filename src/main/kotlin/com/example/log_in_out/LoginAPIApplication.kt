7c28cb5856490d284f88e04eb60fa9d93f27afb5
a783953 <tanws-wa16@student.tarc.edu.my>
2020-03-11 15:18:31 +0800

    google cloud database connection

--- src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
+++ src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
@@ -2,13 +2,20 @@ package com.example.log_in_out
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
 
 @SpringBootApplication
-open class LoginAPIApplication {
-	companion object {
-		@JvmStatic
-		fun main(args: Array<String>) {
-			SpringApplication.run(LoginAPIApplication::class.java, *args)
-		}
+open class LoginAPIApplication : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder? {
+		return application.sources(LoginAPIApplication::class.java)
 	}
+//	companion object {
+//		@JvmStatic
+//		fun main(args: Array<String>) {
+//			SpringApplication.run(LoginAPIApplication::class.java, *args)
+//		}
+//	}
 }
