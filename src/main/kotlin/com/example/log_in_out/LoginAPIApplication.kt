74dce61419a8cb16f44febc6fe2a4f43cf54ff9e
a783953 <tanws-wa16@student.tarc.edu.my>
2020-03-12 15:52:44 +0800

    Google Cloud App Engine-Standard

--- src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
+++ src/main/kotlin/com/example/log_in_out/LoginAPIApplication.kt
@@ -1,21 +1,21 @@
 package com.example.log_in_out
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-open class LoginAPIApplication : SpringBootServletInitializer() {
-	companion object {
-		@JvmStatic
-		fun main(args: Array<String>) {
-			SpringApplication.run(LoginAPIApplication::class.java, *args)
-		}
-	}
-//    fun main(args: Array<String>) {
-//        runApplication<LoginAPIApplication>(*args)
-//    }
+open class LoginAPIApplication : SpringBootServletInitializer()
+
+//	companion object {
+//		@JvmStatic
+//		fun main(args: Array<String>) {
+//			SpringApplication.run(LoginAPIApplication::class.java, *args)
+//		}
+//	}
+fun main(args: Array<String>) {
+    runApplication<LoginAPIApplication>(*args)
 }
+
